990c12a58b64303e8a7ee7e701de5be7bb763ef4
Lưu Mạnh Thắng <98244656+ThangLuu0110@users.noreply.github.com>
2026-07-20 22:52:18 +0700

    feat/handle update task

--- force-app/main/default/lwc/todoListComponent/todoListComponent.js
+++ force-app/main/default/lwc/todoListComponent/todoListComponent.js
@@ -3,11 +3,14 @@ import { refreshApex } from '@salesforce/apex';
 import getAllTodoTasks from '@salesforce/apex/TodoApp_CRUD.getAllTodoTasks';
 import addNewTodoTask from '@salesforce/apex/TodoApp_CRUD.addNewTodoTask';
 import deleteTodoTask from '@salesforce/apex/TodoApp_CRUD.deleteTodoTask';
+import updateTodoTask from '@salesforce/apex/TodoApp_CRUD.updateTodoTask';
 
 export default class TodoListComponent extends LightningElement {
     dateOfToday = new Date().toISOString().split('T')[0];
     title = 'Todo List';
     todoTasksLists = [];
+    selectedTaskId = null;
+    isUpdateMode = false;
     @track taskNameValue = '';
     @track taskDeadline = '';
 
@@ -45,6 +48,35 @@ export default class TodoListComponent extends LightningElement {
        });
     }
 
+    handleEditTask(event) {
+        const taskId = event.currentTarget.dataset.id;
+        const selectedTask = this.todoTasksLists.data?.find((task) => task.Id === taskId);
+
+        if (selectedTask) {
+            this.taskNameValue = selectedTask.Name;
+            this.taskDeadline = selectedTask.Tasks_Deadline__c;
+            this.isUpdateMode = true;
+            this.selectedTaskId = taskId;
+        }
+
+    }
+
+    updateTask() {
+        const taskName = this.template.querySelector('.taskList_form-input.taskName').value;
+        const taskDeadline = this.template.querySelector('.taskList_form-input.taskDeadline').value;
+
+        this.handleResetValues();
+        updateTodoTask({ taskId: this.selectedTaskId, taskName, taskDeadline })
+        .then(() => {
+            this.isUpdateMode = false;
+            return refreshApex(this.todoTasksLists);
+        })
+        .catch((error) => {
+            console.error('Error updating task:', error);
+        });
+
+
+    }
 
     handleTaskNameChange(event) {   
         this.taskNameValue = event.target.value;
